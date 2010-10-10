877322213f290bd0a8e3c382a4dd7378fca03547
Todd Harris <htodd@gattaca.pcbi.upenn.edu>
2010-10-09 20:53:02 -0400

    a little cron to slurp sgd genomic annotations once a week

---
+++ script/mirror_sgd_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Intended to be run as a cron.  Fetch new Saccharomyces annotations once a week.
+# 0 1 * * 0 
+
+# There should be a master config for this.
+DESTINATION_ROOT=/files/cbil/data/cbil/EuPathDB/manualDelivery/FungiDB/saccharomyces_cerevisiae_s288c
+CHROMOSOMES=${DESTINATION_ROOT}/genome/chromosome
+
+DATE=`date +%Y-%m-%d`
+echo ${CHROMOSOMES}/${DATE}
+mkdir -p ${CHROMOSOMES}/${DATE}
+cd ${CHROMOSOMES}/${DATE}
+curl -O http://downloads.yeastgenome.org/chromosomal_feature/saccharomyces_cerevisiae.gff
+
+# Enforce some standardization.
+mv saccharomyces_cerevisiae.gff saccharomyces_cerevisiae_s288c.gff3
